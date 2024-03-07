208bf31e220972fd1a6e661f184bf0c2095250a8
Aula <Aula@CANP-LABTI7-C01>
2024-03-07 17:47:40 -0300

    Adicionar arquivos de projeto.

---
+++ conceitosbasicos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+Console.WriteLine("Informe seu peso: ");
+int peso = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe sua altura: ");
+float altura = float.Parse(Console.ReadLine());
+
+float imc = peso / (altura * altura);
+
+Console.WriteLine($"Para peso {peso} e a altura {altura} o imc calculado foi {imc:f1");
+
+string status = string.Empty;
+if (imc <= 16.9)
+    status = "muito abaixo do peso";
+else if (imc <= 18.4)
+    status = "abaixo do peso";
